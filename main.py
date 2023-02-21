b1febb044814f6a91595beec425ce8e105ad9444
kevinnadar <jesikamaraj@gmail.com>
2023-02-21 17:22:30 +0530

    ..

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import os
 app = Flask(__name__)
 
 WEBSITE_DOMAIN = os.environ.get('WEBSITE_DOMAIN')
+PORT = int(os.environ.get('PORT', 80))
 
-# Defining the home page of our site
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>') 
 def home(path):
     url = request.url.replace(request.host, WEBSITE_DOMAIN)
-    return redirect(url) # some basic inline html
+    return redirect(url) 
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=PORT)
