7b436b4d66d755412a06aecf6f16984ad4f3f989
kevinnadar <jesikamaraj@gmail.com>
2023-04-16 12:43:38 +0530

    ..

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from flask import Flask, redirect , request
+from flask import Flask, redirect, request
 import os
 app = Flask(__name__)
 
 DL_WEBSITE_DOMAIN = os.environ.get('DL_WEBSITE_DOMAIN')
-FS_WEBSITE_DOMAIN = os.environ.get('FS_WEBSITE_DOMAIN')
+FS_WEBSITE_DOMAIN = os.environ.get('FS_WEBSITE_DOMAIN', "https://fs.0x0.st")
 PORT = int(os.environ.get('PORT', 80))
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/dl/<path:path>') 
+@app.route('/dl/')
 def home(path):
     url = request.url.replace(f"{request.host}/dl", DL_WEBSITE_DOMAIN)
-    return redirect(url) 
+    return redirect(url)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/fs/<path:path>') 
-def fs(path):
+
+@app.route('/fs/')
+def fs_home():
     url = request.url.replace(f"{request.host}/fs", FS_WEBSITE_DOMAIN)
-    return redirect(url) 
+    return redirect(url)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
